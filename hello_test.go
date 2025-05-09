eae72850f61b89defa87755f1e017e78df4ef325
Mark Moelter <Mark.Moelter.SA@integraconnect.com>
2025-05-09 09:23:12 -0400

    test: add test for hello func

---
+++ hello_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	got := Hello("Mark")
+	want := "Hello, Mark"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
