3a241963dba41850ac3e3e86289fbf1a1c436b68
Mark Moelter <Mark.Moelter.SA@integraconnect.com>
2025-05-09 16:11:46 -0400

    feat: hello func add default values

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,9 +3,22 @@ package hello
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Mark")
-	want := "Hello, Mark"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Mark")
+		want := "Hello, Mark"
 
+		assertCorrectMessage(t, got, want)	
+	})
+	t.Run("empty string defaults to 'world'", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
