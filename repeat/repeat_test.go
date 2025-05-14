15709daa513232bf9f0ff8d27813a96d5b6919c2
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2025-05-14 13:13:28 -0400

    test: update tests to pass

--- repeat/repeat_test.go
+++ repeat/repeat_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestRepeat(t *testing.T) {
 	t.Run("repeat 'a' five times", func(t *testing.T) {
 		repeated := Repeat("a")
-		expected := "aaaa"
+		expected := "aaaaa"
 
 		if repeated != expected {
 			t.Errorf("expected %q but got %q", expected, repeated)
