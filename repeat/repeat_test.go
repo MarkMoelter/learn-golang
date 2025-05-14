4de15ca9af4541af2c1249aa26e57355858a1ad0
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2025-05-14 13:13:00 -0400

    feat: add repeat func and tests

---
+++ repeat/repeat_test.go
@@ -0,0 +1,14 @@
+package repeat
+
+import "testing"
+
+func TestRepeat(t *testing.T) {
+	t.Run("repeat 'a' five times", func(t *testing.T) {
+		repeated := Repeat("a")
+		expected := "aaaa"
+
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
+}
