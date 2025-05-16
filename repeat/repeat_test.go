20f59b0906140baa00b9774d7f1af9ebfc4c809c
Mark Moelter <Mark.Moelter.SA@integraconnect.com>
2025-05-16 08:58:38 -0400

    test: add Example and benchmark tests

--- repeat/repeat_test.go
+++ repeat/repeat_test.go
@@ -1,6 +1,9 @@
 package repeat
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
 	t.Run("repeat 'a' five times", func(t *testing.T) {
@@ -11,4 +14,17 @@ func TestRepeat(t *testing.T) {
 			t.Errorf("expected %q but got %q", expected, repeated)
 		}
 	})
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
+	}
+}
+
+
+func ExampleRepeat() {
+	repeat := Repeat("z")
+	fmt.Println(repeat)
+	//output: zzzzz
 }
