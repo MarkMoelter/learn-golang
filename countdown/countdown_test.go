46f52ae941933f7b85e13f4d26264bec5a9923fb
Mark Moelter <Mark.Moelter.SA@integraconnect.com>
2025-06-13 12:34:29 -0400

    test: add configurable sleeper tests

--- countdown/countdown_test.go
+++ countdown/countdown_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"reflect"
 	"testing"
+	"time"
 )
 
 func TestCountdown(t *testing.T) {
@@ -40,6 +41,20 @@ func TestCountdown(t *testing.T) {
 
 		if !reflect.DeepEqual(want, spySleepPrinter.Calls) {
 			t.Errorf("wanted calls %v got %v", want, spySleepPrinter.Calls)
+		}
+	})
+}
+
+func TestConfigurableSleeper(t *testing.T) {
+	t.Run("", func(t *testing.T) {
+		sleepTime := 5 * time.Second
+
+		spyTime := &SpyTime{}
+		sleeper := ConfigurableSleeper{sleepTime, spyTime.Sleep}
+		sleeper.Sleep()
+
+		if spyTime.durationSlept != sleepTime {
+			t.Errorf("should have slept for %v but slept for %v", sleepTime, spyTime.durationSlept)
 		}
 	})
 }
