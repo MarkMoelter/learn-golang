b16c270abb927a24143fe2f0a44ffbeb363b123b
Mark Moelter <Mark.Moelter.SA@integraconnect.com>
2025-06-30 11:18:12 -0400

    add: create walk func and tests

--- walk/walk_test.go
+++ walk/walk_test.go
@@ -1,1 +1,28 @@
 package walk
+
+import "testing"
+
+func TestWalk(t *testing.T) {
+	t.Run("check the number of function calls", func(t *testing.T) {
+		const lenDesired = 1
+
+		expected := "Chris"
+		var got []string
+
+		x := struct {
+			Name string
+		}{expected}
+
+		walk(x, func(input string) {
+			got = append(got, input)
+		})
+
+		if len(got) != lenDesired {
+			t.Errorf("wrong number of function calls, got %d want %d", len(got), lenDesired)
+		}
+
+		if got[0] != expected {
+			t.Errorf("got %q, want %q", got[0], expected)
+		}
+	})
+}
