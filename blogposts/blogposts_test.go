7f34eae7c88acfd03b159edb9589098b83cb0198
Mark Moelter <Mark.Moelter.SA@integraconnect.com>
2025-08-08 09:22:00 -0400

    test: add first test

---
+++ blogposts/blogposts_test.go
@@ -0,0 +1,22 @@
+package blogposts_test
+
+import (
+	"testing"
+	"testing/fstest"
+	blogposts "example.com/myproject/blogposts"
+)
+
+func TestBlogPosts(t *testing.T) {
+	t.Run("", func(t *testing.T) {
+		fs := fstest.MapFS{
+			"hello world.md": {Data: []byte("hi")},
+			"hello-world2.md": {Data: []byte("hola")},
+		}
+
+		posts := blogposts.NewPostsFromFS(fs)
+
+		if len(posts) != len(fs) {
+			t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
+		}
+	})
+}
--- reading/reading_test.go
+++
@@ -1,9 +0,0 @@
-package blogposts_test
-
-import "testing"
-
-func TestBlogPosts(t *testing.T) {
-	t.Run("", func(t *testing.T) {
-
-	})
-}
