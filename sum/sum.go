72e3cdb14cea40596f8d9e2bb4ac88b87c673dc6
MarkMoelter <MarkEMoelter@gmail.com>
2026-01-19 15:05:53 -0500

    feat: add bank functionality

--- sum/sum.go
+++ sum/sum.go
@@ -26,3 +26,23 @@ func Reduce[A any](collection []A, f func(A, A) A, initValue A) A {
 
 	return result
 }
+
+type Transaction struct {
+	From string
+	To   string
+	Sum  float64
+}
+
+func BalanceFor(transactions []Transaction, name string) float64 {
+	var balance float64
+	for _, t := range transactions {
+		if t.From == name {
+			balance -= t.Sum
+		}
+		if t.To == name {
+			balance += t.Sum
+		}
+	}
+
+	return balance
+}
--- sum/sum_test.go
+++ sum/sum_test.go
@@ -50,3 +50,46 @@ func TestSumAllTails(t *testing.T) {
 		checkSums(t, got, want)
 	})
 }
+
+func TestReduce(t *testing.T) {
+	t.Run("multiplication of all elements", func(t *testing.T) {
+		multiply := func(x, y int) int {
+			return x * y
+		}
+
+		AssertEqual(t, sum.Reduce([]int{1, 2, 3}, multiply, 1), 6)
+	})
+	t.Run("concatenate strings", func(t *testing.T) {
+		concatenate := func(x, y string) string {
+			return x + y
+		}
+
+		AssertEqual(t, sum.Reduce([]string{"a", "b", "c"}, concatenate, ""), "abc")
+	})
+}
+
+func TestBadBank(t *testing.T) {
+	transactions := []sum.Transaction{
+		{
+			From: "Chris",
+			To:   "Riya",
+			Sum:  100,
+		},
+		{
+			From: "Adil",
+			To:   "Chris",
+			Sum:  25,
+		},
+	}
+
+	AssertEqual(t, sum.BalanceFor(transactions, "Riya"), 100)
+	AssertEqual(t, sum.BalanceFor(transactions, "Chris"), -75)
+	AssertEqual(t, sum.BalanceFor(transactions, "Adil"), -25)
+}
+
+func AssertEqual[T comparable](t *testing.T, got, want T) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %v, want %v", got, want)
+	}
+}
