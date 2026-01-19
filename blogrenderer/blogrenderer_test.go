b6e017cca097cb41ff19d70f1d2e4b87024146e9
Mark Moelter <Mark.Moelter.SA@integraconnect.com>
2026-01-19 10:52:03 -0500

    feat: pull render func into struct

--- blogrenderer/blogrenderer_test.go
+++ blogrenderer/blogrenderer_test.go
@@ -2,6 +2,7 @@ package blogrenderer_test
 
 import (
 	"bytes"
+	"io"
 	"testing"
 
 	"github.com/MarkMoelter/learn-golang/blogrenderer"
@@ -19,13 +20,40 @@ func TestRender(t *testing.T) {
 		}
 	)
 
+	postRenderer, err := blogrenderer.NewPostRenderer()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	t.Run("it converts a single post into HTML", func(t *testing.T) {
 		buf := bytes.Buffer{}
 
-		if err := blogrenderer.Render(&buf, aPost); err != nil {
+		if err := postRenderer.Render(&buf, aPost); err != nil {
 			t.Fatal(err)
 		}
 
 		approvals.VerifyString(t, buf.String())
 	})
+}
+
+func BenchmarkRender(b *testing.B) {
+	var (
+		aPost = blogrenderer.Post{
+			Title: "hello world",
+			Body: "This is a post",
+			Description: "This is a description",
+			Tags: []string{"go", "tdd"},
+		}
+	)
+
+	postRenderer, err := blogrenderer.NewPostRenderer()
+
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	for b.Loop() {
+		postRenderer.Render(io.Discard, aPost)
+	}
 }
