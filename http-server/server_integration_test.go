069807816ef2c87a24b5b96f79db5c0fcd52aed1
MarkMoelter <MarkEMoelter@gmail.com>
2026-02-06 15:32:35 -0500

    feat: move InMemoryPlayerStore to its own file

---
+++ http-server/server_integration_test.go
@@ -0,0 +1,25 @@
+package httpserver_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	httpserver "github.com/MarkMoelter/learn-golang/http-server"
+)
+
+func TestRecordingWinsAndRetrievingThem(t *testing.T) {
+	store := httpserver.NewInMemoryPlayerStore()
+	server := httpserver.PlayerServer{store}
+	player := "Pepper"
+
+	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
+
+	response := httptest.NewRecorder()
+	server.ServeHTTP(response, newGetScoreRequest(player))
+	assertStatus(t, response.Code, http.StatusOK)
+
+	assertResponseBody(t, response.Body.String(), "3")
+}
