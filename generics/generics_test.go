9d413f98ab75fcbd530f4cab99b84450bfe7c88c
MarkMoelter <MarkEMoelter@gmail.com>
2026-02-06 08:38:10 -0500

    fix: use the correct module for the Find function

--- generics/generics_test.go
+++ generics/generics_test.go
@@ -85,7 +85,7 @@ func TestFind(t *testing.T) {
 	t.Run("find first even number", func(t *testing.T) {
 		numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
-		firstEvenNumber, found := sum.Find(numbers, func(x int) bool {
+		firstEvenNumber, found := generics.Find(numbers, func(x int) bool {
 			return x % 2 == 0
 		})
 		AssertTrue(t, found)
